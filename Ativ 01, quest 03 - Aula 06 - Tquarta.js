647a5ec97671e8794c92cfc19b5badce0354ba1f
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-07-03 15:51:18 -0300

    Ativ1, quest 02 - Aula 06-03/06/2023 Tquarta-feira

---
+++ Ativ 01, quest 03 - Aula 06 - Tquarta.js
@@ -0,0 +1,23 @@
+//Criando a array musicas e armazenando 6 nomes de musicas 
+var musicas = ['Da lama ao caos', 'Praieira', 'Se eu não te cantar', '$enhor', 'Planos', 'Quadros'];
+
+//Exibindo no console a quantidade total de itens que tem na minha array musicas e para isso usei o length
+console.log ("A quantidade de itens na array musicas é: " + musicas.length);
+
+//Exibindo no console os itens nas posições 1,2,6,8. OBS: As posições 6 e 8 vão dar undefined porque essas posições não existem na minha array musicas
+var musica_1 = musicas[1];
+console.log("A música na posição 1: " + musica_1);
+var musica_2 = musicas[2];
+console.log("A música na posição 2: " + musica_2);
+var musica_6 = musicas[6];
+console.log("A música na posição 6: " + musica_6);
+var musica_8 = musicas[8];
+console.log("A música na posição 8: " + musica_8);
+
+//Exibindo no console o item na última posição da array musicas
+var musica_5 = musicas[5];
+console.log("A música na última posição é: " + musica_5);
+
+
+
+
