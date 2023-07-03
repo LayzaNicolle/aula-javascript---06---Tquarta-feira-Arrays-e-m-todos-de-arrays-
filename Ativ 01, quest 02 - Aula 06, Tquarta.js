2472ae3fedf5900ec8f27a3a38c1bb3cf67cecbc
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-07-03 15:34:22 -0300

    Ativ1, quest 02 - Aula 06-03/06/2023 Tquarta-feira

---
+++ Ativ 01, quest 02 - Aula 06, Tquarta.js
@@ -0,0 +1,15 @@
+//Criando a array filmes e armazenando nomes de 5 filmes nela
+var filmes = ['Pulp Fiction', 'Forrest Gump', 'Homem Aranha', 'Django', 'Era uma vez em Hollywood'];
+
+//Exibindo no console a quantidade total de itens que tem na minha array e para isso usei o length
+console.log ("A quantidade de itens na array filmes é: " + filmes.length);
+
+//Exibindo no console os itens nas posições 2,4
+var filme_2 = filmes[2];
+console.log("Filme na posição 2: " + filme_2);
+var filme_4 = filmes[4];
+console.log("Filme na posição 4: " + filme_4);
+
+//Exibindo no console o filme na primeira posição
+var filme_0 = filmes[0];
+console.log("Filme na primeira posição: " + filme_0);
