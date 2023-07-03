d04a134badd9f16a074c2782752f076651974574
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-07-03 15:20:47 -0300

    Ativ1, quest 01 - Aula 06 03/07/2023 Tquarta-feira

---
+++ Atividade 01, quest 1 Tquarta.js
@@ -0,0 +1,14 @@
+//criando uma array chamada itens e armazendo valores de 1 a 10
+var itens = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//Exibindo no meu console a quantidade total de itens que tem na minha array e para isso usei o length.
+console.log(itens.length);
+
+//Exibindo no meu console os itens nas posições 1, 3, 5 e 8
+var item_1 = itens[1];
+console.log("Elemento da posição 1 é: " + item_1);
+var item_3= itens[3];
+console.log("Elemento da posição 3 é: " + item_3);
+var item_5= itens[5];
+console.log("Elemento da posição 5 é: " + item_5);
+var item_8= itens[8];
+console.log("Elemento da posição 8 é: " + item_8);
