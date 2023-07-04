595dc2de25b6cdacff12c1f2c5ed5658909a0b3b
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-07-03 21:49:09 -0300

    Ativ 02, quest 04 - Aula 06 - 03/07/2023 Tquarta

---
+++ Ativ 02, quest 04 - Aula 06 - Tquarta.js
@@ -0,0 +1,16 @@
+// Questão 04
+
+//Criando um array e armazenando os nomes de 5 cidades
+var cidades =  ['Recife','Olinda','Caruaru','Garanhus','Campina Grande'];
+
+//Exibindo no console, em ordem crescente, as cidades que estão no array
+cidades.sort();
+console.log(cidades);
+
+//Removendo a cidade que está no início do array
+cidades.shift();
+console.log(cidades);
+
+//Removendo a cidade que está no final do array e exibindo no console as cidades que estão no array
+cidades.pop();
+console.log(cidades);
